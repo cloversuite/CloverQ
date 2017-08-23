2be7f50da3b109903ddd44be94110682a10bb679
alejogagliardi <alejogagliardi@gmail.com>
2017-08-23 12:33:20 -0300

    Add ActorQueueLog and queuelog messages to protocol

---
+++ AkkaActorSystem/ActorQueueLog.cs
@@ -0,0 +1,27 @@
+﻿using Akka.Actor;
+using Akka.Event;
+using ProtocolMessages.QueueLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaActorSystem
+{
+    public class ActorQueueLog : ReceiveActor
+    {
+        private readonly ILoggingAdapter logger;
+        public ActorQueueLog()
+        {
+            logger= Context.GetLogger();
+            
+            Receive<QLMessage>( qlmsg =>
+                {
+                    //logger.Info();
+                });
+        }
+
+
+    }
+}
---
+++ ProtocolMessages/QueueLog/QLMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolMessages.QueueLog
+{
+    public class QLMessage
+    {
+        public DateTime EventDate { get; set; }
+        public string Channel { get; set; }
+        public string Queue { get; set; }
+        public string data1 { get; set; }
+        public string data2 { get; set; }
+        public string data3 { get; set; }
+
+    }
+}
