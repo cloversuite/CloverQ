7dfc996b8aabbf3b38b40ba6cd7b92091dabf872
alejo <alejogagliardi@gmail.com>
2018-07-18 10:39:52 -0300

    Add call time tracking with chronometer class

---
+++ ProtocolMessages/MessageCallHold.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolMessages
+{
+    //comentario
+    public class MessageCallHold : Message
+    {
+
+    }
+}
